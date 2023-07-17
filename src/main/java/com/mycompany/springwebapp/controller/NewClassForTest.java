cd863044d7f5f79d04c84b3cf08a7969623a93c7
JJNSC <whdwls963@gmail.com>
2023-07-17 16:26:24 +0900

    230717 1626 jjk NewClassForTest dto 생성

---
+++ src/main/java/com/mycompany/springwebapp/controller/NewClassForTest.java
@@ -0,0 +1,10 @@
+package com.mycompany.springwebapp.controller;
+
+import lombok.Data;
+
+@Data
+public class NewClassForTest {
+	private int age; 
+	private String name;
+	private String nickname;
+}
